31c3a4b8a8d448e61e42a365b7fcb1b95acc7be6
dnj <157141256+dunajjj@users.noreply.github.com>
2026-06-02 19:50:26 +0200

    Update cfg.lua

--- shared/cfg.lua
+++ shared/cfg.lua
@@ -4,6 +4,20 @@ dnj.scratcheritem = 'dnj_scratcher'
 dnj.scratchhook = 'XXX-XXXXXXX'
 --- @param dnj.weaponblacklist = {}
 dnj.weaponblacklist = { -- zbrane ktorych seriove cislo nejde odstranit
-    -- example :
-    --[["weapon_raypistol"]]
+    -- EXAMPLE :
+    --[[
+    "weapon_raypistol",
+    "weapon_crowbar",
+    "weapon_golfclub",
+    "weapon_hammer",
+    "weapon_hatchet",
+    "weapon_knuckle",
+    "weapon_machete",
+    "weapon_flashlight",
+    "weapon_switchblade",
+    "weapon_nightstick",
+    "weapon_bat",
+    "weapon_poolcue",
+    "weapon_wrench",
+    ]]
 } 
